17e3b85b6f6dd94df6b3ff64c8742ed64b4f6384
nikhil <nikhiljose59@gmail.com>
2024-01-18 13:15:06 +0530

    aa

--- store/models.py
+++ store/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.db.models.signals import post_save
+
 class Category(models.Model):
     name=models.CharField(max_length=200,unique=True)
     created_at=models.DateTimeField(auto_now_add=True)
@@ -38,4 +40,7 @@ class BasketItem(models.Model):
     updated_at=models.DateTimeField(auto_now=True)
     is_active=models.BooleanField(default=True)
 
-
+def create_basket(sender,instance,created,**kwargs):
+        if created:
+            Basket.objects.create(owner=instance)
+post_save.connect(create_basket,sender=User)
